49698ef992ceeea28ce925db9a0b71bf374edda9
geekay009@gmail.com <geekay009@gmail.com>
2020-02-13 15:25:48 +0000

    The logic.py is changed

--- logic.py
+++ logic.py
@@ -5,6 +5,8 @@ elif color == 'teal':
     print("not bad!")
 elif color == 'seafoam':
     print("mediocre")
+elif color == "red":
+    print ("This is my car color")
 elif color == 'pure darkness':
     print("I like how you think")
 else:
